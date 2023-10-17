3d9e3bd4e32dfcd30bcd3916d434be30d8879e4a
Martin Varmuza <mvarmuza@gmail.com>
2023-10-17 18:26:29 +0200

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 'use client';
 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      
       <button onClick={() => {
+        // @ts-expect-error
         window.navigator.usb.requestDevice({
           filters: [],
         });
       }}> webusb pairing with host</button>
 
       <button onClick={() => {
+        // @ts-expect-error
         console.log('trezorConnect', window.TrezorConnect);
+        // @ts-expect-error
         TrezorConnect.init({
           manifest: {
             email: 'meow',
@@ -19,10 +23,13 @@ export default function Home() {
           },
           transport: ['WebUsbTransport'],
           transports: ['WebUsbTransport'],
+        // @ts-expect-error
         }).then((res) => {
           console.log('res init', res);
+        // @ts-expect-error
           TrezorConnect.getAddress({ 
             path: "m/49'/0'/0'/0/0",
+        // @ts-expect-error
           }).then((res2) => {
             console.log('res2', res2);
           })
